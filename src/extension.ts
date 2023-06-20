7568eea1d82d7fe6b0bccfe9075a4347c21edd41
Vitaly Turovsky <vital2580@icloud.com>
2023-06-20 23:51:03 +0300

    prepare changes to next

--- src/extension.ts
+++ src/extension.ts
@@ -62,58 +62,35 @@ export const activate = () => {
             for (const problem of diagnostics) {
                 const pos = problem.range.start
 
-                switch (problem.message) {
-                    // 514 code optionally check source=json
-                    case 'Expected comma': {
-                        if (!enableFixes.insertMissingCommas) continue
-                        // if (character === document.lineAt(line).firstNonWhitespaceCharacterIndex) {
-                        //     console.log('one line')
-                        //     edit.insert(new vscode.Position(line - 1, document.lineAt(line).range.end.character), ',')
-                        //     break
-                        // }
-
-                        edit.insert(pos, ',')
-                        codeActionTitleOverride = 'Insert comma'
-                        break
+                if ((['json', 'jsonc'].includes(problem.source ?? '') && problem.code === 514) || problem.message === 'Expected comma') {
+                    if (!enableFixes.insertMissingCommas) continue
+                    edit.insert(pos, ',')
+                    codeActionTitleOverride = 'Insert comma'
+                } else if ((['json', 'jsonc'].includes(problem.source ?? '') && problem.code === 519) || problem.message === 'Trailing comma') {
+                    if (!enableFixes.removeTrailingCommas) continue
+                    edit.delete(problem.range)
+                    codeActionTitleOverride = 'Remove trailing comma'
+                } else if (problem.message === 'Colon expected') {
+                    if (!enableFixes.insertMissingColon) continue
+                    edit.insert(pos, ':')
+                    codeActionTitleOverride = 'Insert colon'
+                } else if (problem.message === 'Comments are not permitted in JSON.') {
+                    if (!enableFixes.removeComments) continue
+                    edit.delete(problem.range)
+                    codeActionTitleOverride = 'Remove comment'
+                } else if (problem.message === 'Property keys must be doublequoted') {
+                    if (!enableFixes.fixDoubleQuotes) continue
+                    const { start, end } = problem.range
+                    const problemWord = document.getText(problem.range)
+                    const removeQuotes = /^([`']).+\1$/.exec(problemWord)
+                    if (removeQuotes) {
+                        edit.delete(new vscode.Range(start, start.translate(0, 1)))
+                        edit.delete(new vscode.Range(end.translate(0, -1), end))
                     }
 
-                    // these three are quite simple
-                    // 519
-                    case 'Trailing comma':
-                        if (!enableFixes.removeTrailingCommas) continue
-                        edit.delete(problem.range)
-                        codeActionTitleOverride = 'Remove trailing comma'
-                        break
-                    // 515
-                    case 'Colon expected':
-                        if (!enableFixes.insertMissingColon) continue
-                        edit.insert(pos, ':')
-                        codeActionTitleOverride = 'Insert colon'
-                        break
-                    // why no source and code?
-                    case 'Comments are not permitted in JSON.':
-                        if (!enableFixes.removeComments) continue
-                        edit.delete(problem.range)
-                        codeActionTitleOverride = 'Remove comment'
-                        break
-                    case 'Property keys must be doublequoted': {
-                        if (!enableFixes.fixDoubleQuotes) continue
-                        const { start, end } = problem.range
-                        const problemWord = document.getText(problem.range)
-                        const removeQuotes = /^([`']).+\1$/.exec(problemWord)
-                        if (removeQuotes) {
-                            edit.delete(new vscode.Range(start, start.translate(0, 1)))
-                            edit.delete(new vscode.Range(end.translate(0, -1), end))
-                        }
-
-                        edit.insert(start, '"')
-                        edit.insert(end, '"')
-                        codeActionTitleOverride = 'Wrap with double quotes'
-                        break
-                    }
-
-                    default:
-                        break
+                    edit.insert(start, '"')
+                    edit.insert(end, '"')
+                    codeActionTitleOverride = 'Wrap with double quotes'
                 }
             }
         })
@@ -131,7 +108,7 @@ export const activate = () => {
 
     vscode.languages.registerCodeActionsProvider(['json', 'jsonc'], {
         provideCodeActions(document, range, context) {
-            const fixAllRequest = context.only?.contains(vscode.CodeActionKind.SourceFixAll.append('source.fixAll.eslint'))
+            const fixAllRequest = context.only?.contains(vscode.CodeActionKind.SourceFixAll.append('fixAllJson'))
             if (!fixAllRequest) {
                 if (!getExtensionSetting('enableIndividualCodeActions')) return
                 // ensure propose one individual fix
@@ -155,7 +132,7 @@ export const activate = () => {
                     title: 'Fix all JSON problems',
                     // diagnostics
                     edit: workspaceEdit,
-                    kind: vscode.CodeActionKind.SourceFixAll,
+                    kind: vscode.CodeActionKind.SourceFixAll.append('fixAllJson'),
                 },
             ]
         },
